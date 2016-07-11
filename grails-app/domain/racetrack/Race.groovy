67ff27d1ba2fd80c1d982a34075fd7c2fffbd918
= <eddy@Centrals-MacBook-Pro.local>
2016-07-11 15:32:15 -0700

    Add Race contraints

--- grails-app/domain/racetrack/Race.groovy
+++ grails-app/domain/racetrack/Race.groovy
@@ -1,8 +1,15 @@
 package racetrack
 
 class Race {
-	String name	Date startDate	String city	String state	BigDecimal distance	BigDecimal cost	Integer maxRunners = 100000
+	static constraints = {
+		name(blank:false, maxSize:50)
+		startDate()
+		city(blank:false)
+		state(inList:["GA","NC","SC","VA"])
+		distance(min:0.0)
+		cost(min:0.0, max:100.0)
+		maxRunners(min:0, max:100000)
+	}
 
-    static constraints = {
-    }
+	String name	Date startDate	String city	String state	BigDecimal distance	BigDecimal cost	Integer maxRunners = 100000
 }
